233217e9b61cbedc4ec4fa07472767b8b3f57656
TMWHO <handson@tmw.at>
2026-06-22 12:25:48 +0200

    Add files via upload

--- ScopeComponent.h
+++ ScopeComponent.h
@@ -44,6 +44,13 @@ public:
 
 	void paint(juce::Graphics& g) override
 	{
+
+		g.setImageResamplingQuality(juce::Graphics::highResamplingQuality);
+		juce::PathStrokeType stroke(1.6f,
+			juce::PathStrokeType::curved,
+			juce::PathStrokeType::rounded);
+
+
 		g.fillAll(juce::Colours::black);
 
 		g.setColour(juce::Colours::lime);
@@ -73,7 +80,24 @@ public:
 			spectrumPath.lineTo(x, y);
 		}
 
-		g.strokePath(spectrumPath, juce::PathStrokeType(2.0f));
+
+		// fabfilter style pfad
+		g.setColour(juce::Colours::lime.withAlpha(0.2f));
+		g.fillPath(spectrumPath);
+
+		g.setColour(juce::Colours::lime.withAlpha(0.9f));
+		g.strokePath(spectrumPath, juce::PathStrokeType(1.5f));
+
+
+		//soft glow
+		g.setColour(juce::Colours::lime.withAlpha(0.08f));
+		g.strokePath(spectrumPath, juce::PathStrokeType(6.0f));
+
+		g.setColour(juce::Colours::lime.withAlpha(0.4f));
+		g.strokePath(spectrumPath, juce::PathStrokeType(3.0f));
+
+		g.setColour(juce::Colours::lime.withAlpha(1.0f));
+		g.strokePath(spectrumPath, juce::PathStrokeType(1.2f));
 
 		//draw frequ achse
 
@@ -84,7 +108,6 @@ public:
 			20.0f, 50.0f, 100.0f, 200.0f, 500.0f,
 			1000.0f, 2000.0f, 5000.0f, 10000.0f, 20000.0f
 		};
-
 
 		for (auto freq : freqs)
 		{
@@ -108,7 +131,6 @@ public:
 				15,
 				juce::Justification::centred);
 		}
-
 
 	}
 
@@ -135,8 +157,8 @@ private:
 
 	float frequencyToX(float freq, float width)
 	{
-		constexpr float minFreq = 20.0f;
-		constexpr float maxFreq = 20000.0f;
+		constexpr float minFreq = 20.0f;			//def 20.0f
+		constexpr float maxFreq = 20000.0f;			//def 20000.0f
 
 		auto norm = (std::log10(freq) - std::log10(minFreq)) / (std::log10(maxFreq) - std::log10(minFreq));
 
@@ -164,23 +186,55 @@ private:
 
 			auto frequency = minFreq * std::pow(nyquist / minFreq, proportion);
 
-			auto fftDataIndex = juce::jlimit(
+			//auto fftDataIndex = juce::jlimit(
+			//	0,
+			//	fftSize / 2,
+			//	(int)(frequency / nyquist * (fftSize / 2)));
+
+			float fftIndex =
+				frequency / nyquist * (fftSize / 2);
+
+			auto index0 = juce::jlimit(
 				0,
 				fftSize / 2,
-				(int)(frequency / nyquist * (fftSize / 2)));
+				(int)std::floor(fftIndex));
 
-			auto level = juce::jmap(
+			auto index1 = juce::jlimit(
+				0,
+				fftSize / 2,
+				index0 + 1);
+
+			auto frac = fftIndex - (float)index0;
+
+			float magnitude = fftData[index0];
+			float db = juce::Decibels::gainToDecibels(magnitude + 1e-6f);
+			magnitude = fftData[index0] + frac * (fftData[index1] - fftData[index0]);
+
+
+			//smoothing
+			float fftSmoothed[fftSize / 2];
+			juce::zeromem(fftSmoothed, sizeof(fftSmoothed));
+
+			float fftValue =
+				fftData[index0]
+				+ frac * (fftData[index1] - fftData[index0]);
+
+			fftSmoothed[i] = fftSmoothed[i] * 0.8f + fftValue * 0.2f;
+
+			float level = juce::jmap(
 				juce::jlimit(
 					mindB,
 					maxdB,
-					juce::Decibels::gainToDecibels(fftData[fftDataIndex])
+					juce::Decibels::gainToDecibels(fftSmoothed[i] + 1e-6f)
 					- juce::Decibels::gainToDecibels((float)fftSize)),
 				mindB,
 				maxdB,
 				0.0f,
 				1.0f);
 
-			scopeData[i] = level;
+			//level *= std::pow(frequency / 1000.0f, -0.3f);		// zum testen eventuell auskommentieren, auto gain smooth shit
+
+			scopeData[i] = scopeData[i] + 0.15f * (level - scopeData[i]);
 		}
 	}
 
